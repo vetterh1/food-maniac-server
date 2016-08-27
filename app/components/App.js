e18528c1fd68a14bc25b87e4cbe9772421f254cb
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2016-08-27 13:05:07 +0200

    version v1

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Geolocation from './Geolocation';
+import Version from "./Version";
 
+const _version = "0.02";
 
 const App = (props) => <div>
                          <h1>Map Test</h1>
                          <Geolocation />
                          { props.children }
+                         <Version version={_version}/>
                        </div>;
 
 export default App;
