e9e014b5aab43ba8c734f5298d9ab1460cea1eb8
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2016-08-29 18:44:01 +0200

    cleaning

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-import express from 'express'
-import path from 'path'
-import compression from 'compression'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from './app/components/Routes'
+var express = require('express')
+var path = require('path')
+var compression = require('compression')
 
 var app = express()
 
@@ -14,33 +10,9 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, 'dist')))
 
 // send all requests to index.html so browserHistory works
-app.get('*', (req, res) => {
-  match({ routes, location: req.url }, (err, redirect, props) => {
-    if (err) {
-      res.status(500).send(err.message)
-    } else if (redirect) {
-      res.redirect(redirect.pathname + redirect.search)
-    } else if (props) {
-      // hey we made it!
-      const appHtml = renderToString(<RouterContext {...props}/>)
-      res.send(renderPage(appHtml))
-    } else {
-      res.status(404).send('Not Found')
-    }
-  })
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
-
-function renderPage(appHtml) {
-  return `
-    <!doctype html public="storage">
-    <html>
-    <meta charset=utf-8/>
-    <title>(s) Map Test</title>
-    <link rel=stylesheet href=/index.css>
-    <div id=app>${appHtml}</div>
-    <script src="/bundle.js"></script>
-   `
-}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
--- webpack.client.config.for.server.rendering.js
+++ webpack.client.config.for.server.rendering.js
@@ -1,56 +1,18 @@
-/*
- *    USAGE:
- *
- *    1 - Set Dev or Prod env:
- *
- *    - Unix: 
- *            NODE_ENV=production
- *            NODE_ENV=dev
- *        
- *    - Windows: 
- *            SET "NODE_ENV=production"
- *            SET "NODE_ENV=dev"
- *
- *    2 - Build only:
- *
- *            npm run build
- *
- *    3 - Build & Run
- *    
- *            npm start
- *
- *    4 - Server rendering
- *
- *          NODE_SERVER_RENDER=true / set "NODE_SERVER_RENDER=true"
- *          NODE_SERVER_RENDER=false / set "NODE_SERVER_RENDER=false"
- * 
- *    Note on combining: 
- *    - Unix: NODE_ENV=production npm start
- *    - Windows: SET "NODE_ENV=dev" && npm start
- *    
- */
-
-
 var webpack = require('webpack')
 
 console.log("[lve] Start webpack");
 
 var plugins = [];
 if(process.env.NODE_ENV === 'production'){
-  console.log("[lve] Production mode");
   plugins.push(    
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin()
   );
 }
-else {  
-  console.log("[lve] NOT in Production mode");
-}
 
 /* Plugin to inject bundle dist path in index.html */  
 if(process.env.NODE_SERVER_RENDER != 'true'){
-  console.log("[lve] NOT in Server rendering mode");
   var HtmlWebpackPlugin = require('html-webpack-plugin');
   var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
@@ -58,10 +20,7 @@ if(process.env.NODE_SERVER_RENDER != 'true'){
     inject: 'body'
   });
   plugins.push( HTMLWebpackPluginConfig );
-} else {
-    console.log("[lve] NOT in Server rendering mode");
 }
-
 
 
 /*
@@ -72,7 +31,7 @@ if(process.env.NODE_SERVER_RENDER != 'true'){
  */
 var loaders = ['babel-loader?presets[]=es2015&presets[]=react'];
 if(process.env.NODE_ENV != 'production'){
-  // loaders.push('react-hot');
+  loaders.push('react-hot');
 }
 
 
