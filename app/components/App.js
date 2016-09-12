a01336b8f729bdb01d77b64ad0657ae3f1012080
Laurent Vetterhoeffer <laurent.vetterhoeffer@atos.net>
2016-09-12 18:04:22 +0200

    2nd new map test - with places - start working

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Geolocation from './Geolocation';
-import MapContainer from '../containers/basic.jsx';
+import PlacesContainer from '../containers/placesOldFormat.jsx';
 import Version from "./Version";
 
 const _version = "0.14-new-map-branch";
@@ -8,7 +8,7 @@ const _version = "0.14-new-map-branch";
 const App = (props) => <div>
                          <h1>Map Test</h1>
                          <Geolocation />
-                         <MapContainer />
+                         <PlacesContainer />
                          { props.children }
                          <Version version={_version}/>
                        </div>;
