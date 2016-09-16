3ea718688a871ea538b7ce0d34c83bceda9b6035
Laurent Vetterhoeffer <laurent.vetterhoeffer@atos.net>
2016-09-16 18:17:24 +0200

    start work on login

--- app/components/App.js
+++ app/components/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Geolocation from './Geolocation';
-import DisplayPositionFromStore from '../tests/DisplayPositionFromStore.jsx';
-import PlacesContainer from '../containers/PlacesContainer.jsx';
+import { Link } from 'react-router'
+import NavLink from './NavLink'
+import FontAwesome from 'react-fontawesome';
 import Version from "./Version";
 
-const _version = "0.16-new_map_component";
+const _version = "0.12-1";
+
 
 const App = (props) => <div>
-                         <h1>Map Test</h1>
-                         <Geolocation />
-                         <DisplayPositionFromStore />
-                         <PlacesContainer />
-                         { props.children }
-                         <Version version={_version}/>
-                       </div>;
+		<h1>Food Maniac!</h1>
+		<ul role="nav">
+			<li><Link to="/"><FontAwesome name='home' />Home</Link></li>
+			<li><NavLink to="/login"><FontAwesome name='user' />Login</NavLink></li>
+			<li><NavLink to="/where"><FontAwesome name='location-arrow' />Location</NavLink></li>
+		</ul>
+		{ props.children }
+	    <Version version={_version}/>
+	</div>;
 
 export default App;
--- app/components/Root.js
+++ app/components/Root.js
@@ -6,12 +6,17 @@ import App from './App';
 import TestComponent from './TestComponent'
 import TestClass from './TestClass'
 
+import Login from './Login'
+import ChooseLocation from './ChooseLocation'
+
 const NotFound = () => <h1>404 error - This page is not found!</h1>
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={browserHistory}>
-		<Route path="/(:filter)" component={App}>
+		<Route path="/" component={App}>
+			<Route path="/where" component={ ChooseLocation } />
+			<Route path="/login" component={ Login } />
 			<Route path="/testClass" component={ TestClass } />
 			<Route path="/testComponent" component={ TestComponent } />
 			<Route path='*' component={ NotFound } />
