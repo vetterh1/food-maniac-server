252280f5034e9585d1830921df779c3272d7b202
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2016-09-20 22:39:08 +0200

    2

---
+++ server/models/users.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  login: { type: 'String', required: true },
+  first: { type: 'String', required: true },
+  last: { type: 'String', required: true },
+  since: { type: 'Date', default: Date.now, required: true },
+  role: ['String'],
+  nbPosts: { type: 'Number', default: 0, required: true },
+  mark: { type: 'String', required: true },
+});
+
+// on every save, add the date
+userSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.since = currentDate;
+  next();
+});
+
+export default mongoose.model('User', userSchema);
