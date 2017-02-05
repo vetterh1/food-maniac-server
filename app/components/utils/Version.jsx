d6c30db1691735941884f9db37864cd08f085e04
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2017-02-06 00:49:28 +0100

    version in git 4

--- app/components/utils/Version.jsx
+++ app/components/utils/Version.jsx
@@ -6,7 +6,7 @@ const divStyle = {
   padding: '1em 1em',
 };
 
-const Version = props => <div style={divStyle}><br />Version: {__VERSION__}<br />Git version: <iframe src="/gitbranch.txt"><br />Git last commit comment: <iframe src="/gitlastcommitcomment.txt"><br />Git last commit date: <iframe src="/gitlastcommitdate.txt"></iframe></div>;
+const Version = props => <div style={divStyle}><br />Version: {__VERSION__}<br />Git version: <iframe src="/gitbranch.txt"></iframe><br />Git last commit comment: <iframe src="/gitlastcommitcomment.txt"></iframe><br />Git last commit date: <iframe src="/gitlastcommitdate.txt"></iframe></div>;
 
 Version.propTypes = { version: React.PropTypes.string };
 export default Version;
