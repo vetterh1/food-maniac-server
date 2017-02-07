4a38f8304578a623afbbef87f15eb8a0107baa2f
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2017-02-07 17:26:34 +0100

    pm2 fix 2

--- prod_update.sh
+++ prod_update.sh
@@ -3,4 +3,6 @@ pm2 stop all
 pm2 delete boServer foServer
 git pull
 npm install
+NODE_ENV=production  npm run BoServer:build:prod
+NODE_ENV=production  npm run FoServer:build:prod
 pm2 start pm2config.json --env production
