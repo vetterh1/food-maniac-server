4a469b3a99d2111650634e8f95922352a1e0686d
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2017-02-07 17:21:55 +0100

    pm2 fix 1

--- prod_update.sh
+++ prod_update.sh
@@ -1,7 +1,6 @@
 cd ./workspace/mapTest/
-pm2 stop ecosystem.json
+pm2 stop all
+pm2 delete boServer foServer
 git pull
 npm install
-NODE_ENV=production  npm run BoServer:build:prod
-NODE_ENV=production  npm run FoServer:build:prod
-pm2 start ecosystem.json
+pm2 start pm2config.json --env production
