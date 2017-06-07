7bec730639612a2ca1f376fee37cf2267afeccdc
Laurent Vetterhoeffer <vetterh1@yahoo.fr>
2017-06-07 16:49:04 +0200

    [IMPROV] issue 14 WIP 3

--- app/components/pages/MainPageContent.jsx
+++ app/components/pages/MainPageContent.jsx
@@ -7,6 +7,7 @@ import MdStars from 'react-icons/lib/md/stars';
 import MdMap from 'react-icons/lib/md/map';
 import MdRestaurantMenu from 'react-icons/lib/md/restaurant-menu';
 import MdLocalRestaurant from 'react-icons/lib/md/local-restaurant';
+import MdLocationSearching from 'react-icons/lib/md/location-searching';
 
 const styles = {
   divStyle: {
@@ -35,10 +36,10 @@ class MainPageContent extends React.Component {
           </div>
           <div className="homepage-feature-item">
             <a href="/searchItem" style={{ textDecoration: 'none' }}>
-              <div className="homepage-feature-icon"><MdMap size={64} /></div>
+              <div className="homepage-feature-icon"><MdLocationSearching size={64} /></div>
               <h5 className="homepage-feature-title">Search</h5>
               <div className="homepage-feature-detail">
-                <p>Find the best burger around you..</p>
+                <p>Find the best burger around you...</p>
               </div>
             </a>
           </div>
@@ -47,7 +48,7 @@ class MainPageContent extends React.Component {
               <div className="homepage-feature-icon"><MdLocalRestaurant size={64} /></div>
               <h5 className="homepage-feature-title">Check</h5>
               <div className="homepage-feature-detail">
-                <p>Find what&apos;s best here?</p>
+                <p>What&apos;s best where you are?</p>
               </div>
             </a>
           </div>
@@ -84,32 +85,13 @@ class MainPageContent extends React.Component {
           </div>
         </div>
       </div>
-*/
 
 
-/*
-
-            <div><Link className="btn btn-secondary" to="/rate">Rate a dish &raquo;</Link></div>
+              <div><Link className="btn btn-secondary" to="/rate">Rate a dish &raquo;</Link></div>
               <div><Link className="btn btn-secondary" to="/searchItem">Search for a dish &raquo;</Link></div>
               <div><Link className="btn btn-secondary" to="/searchItem">Check a place &raquo;</Link></div>
 
-
-      <Container fluid>
-        <Row>
-          <Col className="md-4" >
-            <h2>Rate</h2>
-            <p>Give stars to the pizza you are currently eating!</p>
-          </Col>
-          <Col className="md-4" >
-            <h2>Search</h2>
-            <p>Find the best burger around you...</p>
-          </Col>
-          <Col className="md-4" >
-            <h2>Check</h2>
-            <p>Find what&apos;s best here?</p>
-          </Col>
-        </Row>
-      </Container>
 */
+
 
 export default MainPageContent;
