8cf08f0642b2ce8c81920d5e71a8768b91204d5b
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2018-10-08 19:10:56 +0200

    cleaning

--- prod_update.sh
+++ prod_update.sh
@@ -1,13 +1,7 @@
-cd ./workspace/mapTest/
-rm distFoServer/bundle.*
-rm distFoServer/main.*
-rm distFoServer/manifest.*
-rm distFoServer/styles.*
-rm distFoServer/vendor.*
-pm2 stop all
-pm2 delete boServer foServer
+cd ./workspace/food-maniac-server/
+pm2 stop boServer
+pm2 delete boServer
 git pull
 npm install
 NODE_ENV=production  npm run BoServer:build:prod
-NODE_ENV=production  npm run FoServer:build:prod
 pm2 start pm2config.json --env production
