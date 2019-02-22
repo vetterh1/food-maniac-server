8fa30bfec70713e9e2be7de35345889f91fe6128
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2019-02-22 14:33:34 +0100

    minor changes

--- prod_update_server.sh
+++ prod_update_server.sh
@@ -3,5 +3,5 @@ pm2 stop boServer
 pm2 delete boServer
 git pull
 npm install
-NODE_ENV=production  npm run BoServer:build:prod
+NODE_ENV=production  npm run build:prod
 pm2 start pm2config.json --env production
